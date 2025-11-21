2018e1034d7f144e67698aebc687ef0091deb1b3
BirukGoitom <birukgoitom34@gmail.com>
2025-11-21 11:23:53 -0800

    added a flag to fix the invalid massage

--- calander_converter.py
+++ calander_converter.py
@@ -2,6 +2,8 @@
 
 calander = int(input("Enter:\n1.Convert GC to EC\n2.Convert EC to GC: \nchoice: "))
 
+flag = False
+
 
 #GC to EC conversion
 if calander == 1:
@@ -11,6 +13,7 @@ if calander == 1:
 
     #year conversion
     if gc_year < 1:
+        flag = True
         print("Invalid year for Gregorian calendar.")
     elif (gc_month < 9 or (gc_month == 9 and gc_day < 11)) or gc_day == 11 and ((gc_year + 1) % 4 == 0 and ((gc_year + 1) % 100 != 0 or (gc_year + 1) % 400 == 0)):
         ec_year = gc_year - 8
@@ -20,6 +23,7 @@ if calander == 1:
     #month conversion
 
     if gc_month < 1 or gc_month > 12:
+        flag = True
         print("Invalid month for Gregorian calendar.")
     elif (gc_month == 9 and gc_day == 11) and ((gc_year + 1) % 4 == 0 and ((gc_year + 1) % 100 != 0 or (gc_year + 1) % 400 == 0)):
         ec_month = 13
@@ -53,6 +57,7 @@ if calander == 1:
     #day conversion
     
     if gc_day < 1 or gc_day > 31:
+        flag = True
         print("Invalid day for Gregorian calendar.")
     elif gc_month == 1:
         ec_day = gc_day - 8
@@ -63,6 +68,7 @@ if calander == 1:
         if ec_day < 1:
             ec_day += 30
     elif gc_month == 2 and gc_day >= 29:
+        flag = True
         print("Invalid day for February in Gregorian calendar.")
     elif gc_month == 2:
         ec_day = gc_day - 7
@@ -114,7 +120,8 @@ if calander == 1:
         ec_day = gc_day - 9
         if ec_day < 1:
             ec_day += 30
-    print(f"The Ethiopian calander is: {ec_day}/{ec_month}/{ec_year}")
+    if flag == False:
+        print(f"The Ethiopian calander is: {ec_day}/{ec_month}/{ec_year}")
 
 #EC to GC conversion
 
@@ -126,7 +133,8 @@ elif calander == 2:
     #year conversion
 
     if ec_year < 1:
-        print("Invalid Ethiopian year.")
+        flag = True
+        print("Invalid year for Ethiopian calender.")
 
     elif ec_month < 4 or (ec_month == 4 and ec_day < 23):
         gc_year = ec_year + 7
@@ -136,12 +144,10 @@ elif calander == 2:
     #month conversion
 
     if ec_month < 1 or ec_month > 13:
-        print("Invalid month for Gregorian calendar.")
-    if ec_day < 1 or ec_day > 30:
-            print("Invalid day for Ethiopian calendar.")
+        flag = True
+        print("Invalid month for Ethiopian calendar.")
     elif (ec_month == 6 and ec_day == 21) and ((ec_year + 1) % 4 == 0 and ((ec_year + 1) % 100 != 0 or (ec_year + 1) % 400 == 0)):
             gc_month = 2
-
     elif (ec_month == 4 and ec_day >= 23) or (ec_month == 5 and ec_day <= 23):
         gc_month = 1
     elif (ec_month == 5 and ec_day >= 24) or (ec_month == 6 and ec_day <= 21):
@@ -172,7 +178,8 @@ elif calander == 2:
     #day conversion
 
     if ec_day < 1 or ec_day > 30:
-        print("Invalid day for Gregorian calendar.")
+        flag = True
+        print("Invalid day for Ethiopian calendar.")
     elif ec_month == 1 and (gc_year % 100 != 0 or gc_year % 400 == 0):
         gc_day = ec_day + 11
         if gc_day > 30:
@@ -201,7 +208,6 @@ elif calander == 2:
          gc_day = ec_day + 8
          if gc_day > 28:
             gc_day -= 29
-
     elif ec_month == 6:
         gc_day = ec_day + 7
         if gc_day > 28:
@@ -233,10 +239,12 @@ elif calander == 2:
     elif ec_month == 13 and ((ec_year + 1) % 4 == 0 and ((ec_year + 1) % 100 != 0 or (ec_year + 1) % 400 == 0)):
         gc_day = ec_day + 5
     elif ec_month == 13 and ec_day > 5:
+        flag = True
         print("Invalid day for Ethiopian 13th month.")
     elif ec_month == 13:
         gc_day = ec_day + 5
-    print(f"The Gregorian calander is: {ec_day}/{ec_month}/{ec_year}")
+    if flag == False:
+        print(f"The Gregorian calander is: {ec_day}/{ec_month}/{ec_year}")
 else:
     print("Invalid choice. Please select 1 or 2.")
         
